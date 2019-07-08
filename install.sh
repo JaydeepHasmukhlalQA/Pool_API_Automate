2544afc67db933b87522d99652f3230a0484894e
Jaydeep <jaydeep.hasmukhlal@academytrainee.com>
2019-07-08 09:01:47 -0700

    Changed git project.

--- install.sh
+++ install.sh
@@ -9,12 +9,13 @@ sudo apt-get install -y git
 sudo apt-get install -y nodejs
 sudo apt-get install -y mongodb
 
-sudo git clone https://github.com/Nboaram/TeamAPoolProjectBackend.git
-sudo cd TeamAPoolProjectBackend
+sudo cp ./poolapi.service /etc/systemd/system/
+sudo systemctl daemon-reload
+
+sudo git clone https://github.com/DimitriosKan/TeamB_Pool_Project_API.git
+sudo mv TeamB_Pool_Project_API/ ../TeamB_Pool_Project_API/
+sudo cd TeamB_Pool_Project_API
 sudo git checkout Developer
 sudo npm install
 
-sudo cp ./mongodb.service /etc/systemd/system/
 sudo systemctl daemon-reload
-
-sudo systemctl start poolapi
