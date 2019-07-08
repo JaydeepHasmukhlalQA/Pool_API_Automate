e0da0c1c9607a8c28491bf35eac8927cdcf55ecb
Jaydeep <jaydeep.hasmukhlal@academytrainee.com>
2019-07-08 08:23:25 -0700

    Added git.

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt-get update -y && apt-get upgrade -y
 sudo apt-get install curl python-software-properties
 sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 
+sudo apt-get install -y git
 sudo apt-get install -y nodejs
 sudo apt-get install -y mongodb
 
