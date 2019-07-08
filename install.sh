465e79353f8a91ad2bec98a27eb0a269128e7c71
Jaydeep <jaydeep.hasmukhlal@academytrainee.com>
2019-07-08 08:19:07 -0700

    Created automate and service for API.

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt-get update -y && apt-get upgrade -y
+
+sudo apt-get install curl python-software-properties
+sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+
+sudo apt-get install -y nodejs
+sudo apt-get install -y mongodb
+
+sudo cp ./mongodb.service /etc/systemd/system/
+sudo systemctl daemon-reload
+
+sudo git clone https://github.com/Nboaram/TeamAPoolProjectBackend.git
+sudo cd TeamAPoolProjectBackend
+sudo git checkout Developer
+
+sudo npm install
+
+sudo systemctl start poolapi
+
+
